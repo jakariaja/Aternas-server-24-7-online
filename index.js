8e6abe4c769c99b459531cb9cf011cb4b51196ad
jakariaja <116480223+jakariaja@users.noreply.github.com>
2026-09-03 22:26:25 +0600

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function createBot() {
   username: BOT_USERNAME,
   offline: true,
   skipPing: true,
-  version: false // Bypass strict version check
+  version: false,
 });
   
   client.on("spawn", () => {
